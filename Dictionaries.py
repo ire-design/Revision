0d096aa03e46e43acb94589fb538025fd630e2f8
Irene Musau <irenemwikalii04@gmail.com>
2024-10-01 15:58:08 +0300

    done

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+my_dict = {
+            "key1": "value1",
+            "key2": "value2",
+            "name": "musau"
+          }
+print(my_dict)
+
+#access data from the dictionary
+print(my_dict["key2"])
+
+print(my_dict.get("name"))
