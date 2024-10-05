dc6728d91e7342f15034773e8964fd8088943f62
Irene Musau <irenemwikalii04@gmail.com>
2024-10-05 08:07:39 +0300

    done

--- fnSynatax.py
+++ fnSynatax.py
@@ -3,5 +3,25 @@ def my_function(param):
     return param + 1
 
 my_function_return_value = my_function(1)
-
 print(my_function_return_value)
+
+
+#arguments
+##default
+def say_hi(name= 'engineer'):
+    print (f"Hi there, {name}")
+    
+say_hi()
+
+say_hi("musau!")
+
+# Return Values
+def stylish_barber ():
+    best_hairstyle = "Bob Ross"
+    return "Jean"
+    return best_hairstyle
+    print(best_hairstyle)
+    
+
+print(stylish_barber())
+
