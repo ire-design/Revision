b9821e37a4a6d7cc517eda0488b60afdc8c8c958
Irene Musau <irenemwikalii04@gmail.com>
2024-10-23 23:14:27 +0300

    complete

--- arrays.js
+++ arrays.js
@@ -1,10 +1,28 @@
-const library = {
-    section: "Science",
-    books: [
-      { title: "Physics Fundamentals", authors: ["Dr. A", "Dr. B"] },
-      { title: "Chemistry Basics", authors: ["Dr. C"] }
+// const library = {
+//     section: "Science",
+//     books: [
+//       { title: "Physics Fundamentals", authors: ["Dr. A", "Dr. B"] },
+//       { title: "Chemistry Basics", authors: ["Dr. C"] }
+//     ]
+//   };
+
+//   const author =library.books[0].authors[1]
+//   console.log(author)
+
+  const school = {
+    name: "Green Valley High",
+    classes: [
+      { subject: "Math", students: ["Alice", "Bob", "Charlie"] },
+      { subject: "History", students: ["David", "Eve", "Frank"] }
     ]
   };
+  
+  let y=''
+  let f =''
+  for (let i in school.classes){
+    y += school.classes[i]
+    for (let j in school.classes[i].students ){
+        f += school.class[i].students[j]
 
-  const author =library.books[0].authors[1]
-  console.log(author)
+    }
+  }
