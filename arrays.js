aaa25cae7114edef6eb30005e7de57061644d011
Irene Musau <irenemwikalii04@gmail.com>
2024-10-23 15:37:13 +0300

    complete

---
+++ arrays.js
@@ -0,0 +1,10 @@
+const library = {
+    section: "Science",
+    books: [
+      { title: "Physics Fundamentals", authors: ["Dr. A", "Dr. B"] },
+      { title: "Chemistry Basics", authors: ["Dr. C"] }
+    ]
+  };
+
+  const author =library.books[0].authors[1]
+  console.log(author)
