4e94371ab3980585d058989a81b73dfbd79a9d05
Irene Musau <irenemwikalii04@gmail.com>
2024-10-30 06:35:15 +0300

    complete

--- sort.py
+++ sort.py
@@ -7,3 +7,16 @@ print(my_list)
 my_list = ['Cabbage', 'Apple', 'Banana', 'Potato']
 my_list.sort()
 print(my_list)
+
+
+
+my_list = ['This is a long sentence', 'Word', 'z']
+# What if we want to sort by the length of the string?
+# We can use the key attribute to tell the sort function to sort using the len function.
+my_list.sort(key = len)
+print(my_list)
+
+# If we want to sort in descending order we can pass in the reverse parameter into sort.
+my_list = ['This is a long sentence', 'Word', 'z']
+my_list.sort(key = len, reverse=True)
+print(my_list)
