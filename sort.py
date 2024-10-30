04fa12739ac426883103783d6a65138f992f3c89
Irene Musau <irenemwikalii04@gmail.com>
2024-10-30 06:33:22 +0300

    complete

---
+++ sort.py
@@ -0,0 +1,9 @@
+# if we sort numbers, they will be sorted in ascending order
+my_list = [3, 6, 4, 2, 1, 5]
+my_list.sort()
+print(my_list)
+
+# If we sort strings, they will be sorted in alphanumeric order
+my_list = ['Cabbage', 'Apple', 'Banana', 'Potato']
+my_list.sort()
+print(my_list)
