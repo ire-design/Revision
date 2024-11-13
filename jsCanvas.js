9f367eac5cd91175a9abadeee2b24d8f09c1bc76
Irene Musau <irenemwikalii04@gmail.com>
2024-11-13 09:21:51 +0300

    revision

---
+++ jsCanvas.js
@@ -0,0 +1,11 @@
+/*Array Iteration*/
+const Users = [
+    {
+        firstName: 'Nicky',
+        lastName: "Morgan",
+        favColor: 'Blue',
+        favAnimal: 'Jaguar',
+        personalQuote: 'Go hard or go home'
+    },
+    
+]
