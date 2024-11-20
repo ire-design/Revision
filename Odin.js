221737d6c7c0582c302941349935f606fdbe4440
Irene Musau <irenemwikalii04@gmail.com>
2024-11-20 07:01:03 +0300

    revision

--- Odin.js
+++ Odin.js
@@ -17,3 +17,23 @@ class Book {
   const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
   console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
   
+
+
+// inception.describe(); // "Inception directed by Christopher Nolan, released in 2010, already watched"
+
+  class Movie {
+    constructor(title, director, year, isWatched) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.isWatched = isWatched;
+    }
+
+    info() {
+        const watchStatus = this.isWatched ? "already watched" : "not watched yet"
+        return `${this.title} directed by ${this.director}, released in ${this.year}, ${watchStatus}` 
+    }
+  }
+
+  const inception = new Movie("Inception", "Christopher Nolan", 2010, true)
+  console.log(inception.info())
